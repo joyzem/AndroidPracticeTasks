07202ae96ef3118fbba209472b9fee90049dd66f
joyzem <74488514+joyzem@users.noreply.github.com>
2021-07-16 08:57:04 +0300

    Button

---
+++ app/src/main/java/com/example/practicetask1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.practicetask1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Snackbar.make(it, "Hello!", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
