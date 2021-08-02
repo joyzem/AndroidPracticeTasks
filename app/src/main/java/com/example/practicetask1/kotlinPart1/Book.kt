d38633e9c9ea1f7a859312b23164a394e8bbbec7
Rodion Gazizov <rodion.gazizov@bk.ru>
2021-08-02 16:55:36 +0300

    Requested changes

--- app/src/main/java/com/example/practicetask1/kotlinPart1/Book.kt
+++ app/src/main/java/com/example/practicetask1/kotlinPart1/Book.kt
@@ -2,10 +2,18 @@ package com.example.practicetask1.kotlinPart1
 
 class Book(override var price: Float, override val wordCount: Int) : Publication {
 
+    companion object {
+        const val FLASH_FICTION_WORDS_LIMIT = 1000
+        const val SHORT_STORY_WORDS_LIMIT = 7500
+        const val FLASH_FICTION = "Flash fiction"
+        const val SHORT_STORY = "Short story"
+        const val NOVEL = "Novel"
+    }
+
     override fun getType(): String = when {
-        wordCount < 1000 -> "Flash Fiction"
-        wordCount < 7500 -> "Short Story"
-        else -> "Novel"
+        wordCount < FLASH_FICTION_WORDS_LIMIT -> FLASH_FICTION
+        wordCount < SHORT_STORY_WORDS_LIMIT -> SHORT_STORY
+        else -> NOVEL
     }
 
     override fun equals(other: Any?): Boolean {
