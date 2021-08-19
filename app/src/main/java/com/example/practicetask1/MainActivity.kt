b4c1e13f1fc45b86969e3c57afebc9abf57fd90d
Rodion Gazizov <rodion.gazizov@bk.ru>
2021-08-19 11:53:46 +0300

    Profile layout done

--- app/src/main/java/com/example/practicetask1/MainActivity.kt
+++ app/src/main/java/com/example/practicetask1/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.practicetask1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.google.android.material.snackbar.Snackbar
+import androidx.core.view.get
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_PracticeTask1)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_layout)
+        val bottomNavigationMenu: BottomNavigationView = findViewById(R.id.bottom_nav_menu)
+        bottomNavigationMenu.apply {
+            background = null
+            menu.getItem(2).isEnabled = false
+            menu.getItem(4).isChecked = true
+        }
     }
 }
