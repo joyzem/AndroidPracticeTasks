83a41b398df772b45e885a077f6a4f02eb39bceb
Rodion Gazizov <rodion.gazizov@bk.ru>
2021-08-24 09:41:46 +0300

    activity bottom nav refactoring

--- app/src/main/java/com/example/practicetask1/MainActivity.kt
+++ app/src/main/java/com/example/practicetask1/MainActivity.kt
@@ -2,18 +2,41 @@ package com.example.practicetask1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.example.practicetask1.fragments.HelpFragment
+import com.example.practicetask1.fragments.ProfileFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_PracticeTask1)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bottomNavigationMenu: BottomNavigationView = findViewById(R.id.bottom_nav_menu)
-        bottomNavigationMenu.apply {
+
+        val navigationBarView = findViewById<NavigationBarView>(R.id.bottom_nav_menu).apply {
             background = null
             menu.getItem(2).isEnabled = false
-            menu.getItem(4).isChecked = true
+            menu.getItem(2).isChecked = true
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.profile-> navToFragment(ProfileFragment())
+                }
+                true
+            }
         }
+
+        val fab = findViewById<FloatingActionButton>(R.id.help_fab)
+        fab.setOnClickListener {
+            navToFragment(HelpFragment())
+            navigationBarView.menu.getItem(2).isChecked = true
+        }
+    }
+
+    private fun navToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .setReorderingAllowed(true)
+            .commit()
     }
 }
